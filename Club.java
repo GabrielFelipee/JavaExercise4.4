8d6195ad614aeb5d73c18887405b0cdc51f4aa9b
GabrielFelipee <38012034+GabrielFelipee@users.noreply.github.com>
2019-04-16 17:07:19 -0300

    Add files via upload

---
+++ Club.java
@@ -0,0 +1,42 @@
+/**
+ * Store details of club memberships.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.ArrayList; 
+
+public class Club
+{
+    private String clubName;
+    private ArrayList<Membership> listMembers;
+    /**
+     * Constructor for objects of class Club
+     */
+    public Club()
+    {
+        clubName = "Paradise City";
+        listMembers = new ArrayList<Membership>();
+    }
+
+    /**
+     * Add a new member to the club's list of members.
+     * @param member The member object to be added.
+     */
+    public void join(Membership member)
+    {
+    }
+
+    /**
+     * @return The number of members (Membership objects) in
+     *         the club.
+     */
+    public int numberOfMembers()
+    {   
+        int totalOfMembers;
+        
+        totalOfMembers = listMembers.size();
+
+        return totalOfMembers;
+    }
+}
